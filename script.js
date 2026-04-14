153cd65155196c5a4d4412168b652f47ee9f7571
Santiago Sieghart <santiagosieghart@gmail.com>
2026-04-14 20:58:10 -0300

    Input archivos y lector basicos

--- script.js
+++ script.js
@@ -5,8 +5,26 @@ dropZone.addEventListener('dragover', (e) => {
 dropZone.addEventListener('drop', (e) => {
     var _a;
     e.preventDefault();
-    const files = (_a = e.dataTransfer) === null || _a === void 0 ? void 0 : _a.files;
-    console.log(files);
+    const file = (_a = e.dataTransfer) === null || _a === void 0 ? void 0 : _a.files[0];
+    if (!file)
+        return;
+    leerArchivo(file);
 });
+const inputArchivo = document.getElementById('input-archivo');
+inputArchivo.addEventListener('input', (e) => {
+    var _a;
+    const file = (_a = inputArchivo.files) === null || _a === void 0 ? void 0 : _a[0];
+    if (!file)
+        return;
+    leerArchivo(file);
+});
+function leerArchivo(file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        var _a;
+        console.log((_a = e.target) === null || _a === void 0 ? void 0 : _a.result);
+    };
+    reader.readAsText(file);
+}
 export {};
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -6,6 +6,32 @@ dropZone.addEventListener('dragover', (e) => {
 
 dropZone.addEventListener('drop', (e) => {
   e.preventDefault()
-  const files = e.dataTransfer?.files
-  console.log(files)
+
+  const file = e.dataTransfer?.files[0]
+
+  if (!file) return
+
+  leerArchivo(file)
 })
+
+const inputArchivo = document.getElementById(
+  'input-archivo',
+) as HTMLInputElement
+
+inputArchivo.addEventListener('input', (e) => {
+  const file = inputArchivo.files?.[0]
+
+  if (!file) return
+
+  leerArchivo(file)
+})
+
+function leerArchivo(file: File) {
+  const reader = new FileReader()
+
+  reader.onload = function (e) {
+    console.log(e.target?.result)
+  }
+
+  reader.readAsText(file)
+}
